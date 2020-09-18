45be92fe5175f970bdf472313b4b01228a5d8de5
vladilen597 <45649644+vladilen597@users.noreply.github.com>
2020-09-19 00:55:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -48,10 +48,7 @@ connection.end(err =>{
         console.log("\nDatabase connection closed");
     }
 });
-<<<<<<< HEAD
 
 app.get('/', (require, result) => {
     result.send("Имя: " + blyat);
 });  
-=======
->>>>>>> 7ca206c365a594a045337e128cb801ad5ae460a6
